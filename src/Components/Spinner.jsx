d21be6ac0990381832cd7df41ba18a5303e286e2
dfazil <dfazil.work@gmail.com>
2022-01-16 14:14:48 +0400

    Changed the spinner

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -12,22 +12,15 @@ function Spinner() {
 export default Spinner
 
 const SpinnerStyle = styled.div`
-  body { margin-top: 100px; background-color: #137b85; color: #fff; text-align:center; }
-
-  h1 {
-    font: 2em 'Roboto', sans-serif;
-    margin-bottom: 40px;
-  }
-
   #loading {
     display: flex;
     justify-content: center;
-    margin: 15rem auto;
+    margin: 20rem auto 0 auto;
     width: 66px;
     height: 66px;
     border: 5px solid #ffe01b;
     border-radius: 50%;
-    border-top-color: #fff;
+    border-top-color: transparent;
     animation: spin 1s ease-in-out infinite;
     -webkit-animation: spin 1s ease-in-out infinite;
   }
